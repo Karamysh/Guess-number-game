dc3236e1ae1c5749cc6b6ca408504a687d1f238e
Bitvinus <dezenu@ya.ru>
2022-10-09 18:34:29 +0300

    feat: add a number input logic, add alert message

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,12 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native';
 
-function PrimaryButton({ children }) {
+function PrimaryButton({ children, onPress }) {
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
         style={styles.buttonInnerContainer}
         android_ripple={{ color: '#640233' }}
+        onPress={onPress}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
